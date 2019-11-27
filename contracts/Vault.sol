2d1ac46542d74bad0ac3b61f7d4571026af6f486
Christopher Maree <christopher.maree@gmail.com>
2019-11-27 10:29:01 +0200

    Refactored logic for bulk settlement into vault

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -5,28 +5,60 @@ import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
 
 contract Vault is Initializable {
-
     ERC20 token;
 
     address registry;
 
-    modifier onlyRegistry(){
-        require(msg.sender == registry,"Can only be called by registry");
+    modifier onlyRegistry() {
+        require(msg.sender == registry, "Can only be called by registry");
         _;
     }
 
-    function initialize(address _tokenAddress, address _unicoinRegistry) public initializer {
+    function initialize(address _tokenAddress, address _unicoinRegistry)
+        public
+        initializer
+    {
         token = ERC20(_tokenAddress);
         registry = _unicoinRegistry;
     }
 
-    function canBidderPay(address _address, uint256 _amount) public view returns (bool) {
+    function canBidderPay(address _address, uint256 _amount)
+        public
+        view
+        returns (bool)
+    {
         uint256 userBalance = token.balanceOf(_address);
         uint256 userContractAproval = token.allowance(_address, address(this));
         return (userBalance >= _amount) && (userContractAproval >= _amount);
     }
 
-    function settlePayment(address _sender, address _reciver, uint256 _amount) public onlyRegistry returns (uint256) {
+    function settlePayment(address _sender, address _reciver, uint256 _amount)
+        public
+        onlyRegistry
+        returns (uint256)
+    {
         token.transferFrom(_sender, _reciver, _amount);
+    }
+
+    function settleBulkPayment(
+        address _sender,
+        address _authorAddress,
+        address[] memory _contributorAddresses,
+        uint256[] memory _contributorWeightings,
+        uint256 _paymentAmount
+    ) public returns (bool) {
+        uint256 totalPaidToContributors = 0;
+        for (uint256 i = 0; i < _contributorAddresses.length; i++) {
+            uint256 amountToPay = (_paymentAmount * _contributorWeightings[i]) /
+                1e2;
+            totalPaidToContributors += _contributorWeightings[i];
+            token.transferFrom(_sender, _contributorAddresses[i], amountToPay);
+        }
+
+        uint256 authorAmount = ((1e2 - totalPaidToContributors) *
+            _paymentAmount) /
+            1e2;
+        token.transferFrom(_sender, _authorAddress, authorAmount);
+        return true;
     }
 }
